3da9525dc89c8018db5a8b72629cfa5dba08aa9d
Sambit Sankalp <sambit.sankalp.official@gmail.com>
2023-02-19 21:15:26 +0530

    fix:update read and write contracts

--- client/src/contexts/useContract/writeContract.js
+++ client/src/contexts/useContract/writeContract.js
@@ -1,48 +1,65 @@
-  const createSeller = async(sellerId,contract,account)=>{
-    if (!contract) {
-        return false;
-      }
-      const res = await contract.methods.createSeller(sellerId).send({from:account});
-      return res;
-
-  }
-
-  const createNFT = async(contract,tokenURI,sellerId,productId,customer,expiry,imageURI,account)=>{
-    if (!contract) {
-        return false;
-      }
-      const res = await contract.methods.createNFT(tokenURI,sellerId,productId,customer,expiry,imageURI).send({from:account});
-      return res;
-  }
-  const resell = async(contract,to,tokenId,sellerId,account)=>{
-    if (!contract) {
-        return false;
-      }
-      const res = await contract.methods.resell(to,tokenId,sellerId).send({from:account})
-      return res;
-
-  }
-
-  
-const verify = async(contract,account,productId,sellerId,tokenId)=>{
+// createSeller
+const createSeller = async (sellerId, contract, account) => {
   if (!contract) {
     return false;
   }
-  const res = await contract.methods.verifyOwnership(productId,sellerId,tokenId).send({from:account});
+  const res = await contract.methods
+    .createSeller(sellerId)
+    .send({ from: account });
   return res;
-}
+};
 
-const claim = async(contract,account,sellerId,tokenId)=>{
+// createNFT
+const createNFT = async (
+  contract,
+  tokenURI,
+  sellerId,
+  productId,
+  customer,
+  expiry,
+  imageURI,
+  account
+) => {
   if (!contract) {
     return false;
   }
-  const res = await contract.methods.claimNMint(sellerId,tokenId).send({from:account})
+  const res = await contract.methods
+    .createNFT(tokenURI, sellerId, productId, customer, expiry, imageURI)
+    .send({ from: account });
   return res;
-}
-export{
-  createSeller,
-  createNFT,
-  resell,
-  verify,
-  claim
-}
+};
+
+// resell
+const resell = async (contract, to, tokenId, sellerId, account) => {
+  if (!contract) {
+    return false;
+  }
+  const res = await contract.methods
+    .resell(to, tokenId, sellerId)
+    .send({ from: account });
+  return res;
+};
+
+// verify
+const verify = async (contract, account, productId, sellerId, tokenId) => {
+  if (!contract) {
+    return false;
+  }
+  const res = await contract.methods
+    .verifyOwnership(productId, sellerId, tokenId)
+    .send({ from: account });
+  return res;
+};
+
+// claim
+const claim = async (contract, account, sellerId, tokenId) => {
+  if (!contract) {
+    return false;
+  }
+  const res = await contract.methods
+    .claimNMint(sellerId, tokenId)
+    .send({ from: account });
+  return res;
+};
+
+export { createSeller, createNFT, resell, verify, claim };
